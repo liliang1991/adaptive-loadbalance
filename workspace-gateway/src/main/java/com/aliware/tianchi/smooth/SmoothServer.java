18b037502b1045ea8b17d56dca9b5d3981f2e861
moon <liliang_68@126.com>
2019-06-18 21:19:51 +0800

    增加pom 依赖

---
+++ workspace-gateway/src/main/java/com/aliware/tianchi/smooth/SmoothServer.java
@@ -0,0 +1,39 @@
+package com.aliware.tianchi.smooth;
+
+public class SmoothServer {
+    private String ip;
+
+    private int weight;
+
+    private int curWeight;
+
+    public SmoothServer(String ip, int weight, int curWeight) {
+        this.ip = ip;
+        this.weight = weight;
+        this.curWeight = curWeight;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getCurWeight() {
+        return curWeight;
+    }
+
+    public void setCurWeight(int curWeight) {
+        this.curWeight = curWeight;
+    }
+}
