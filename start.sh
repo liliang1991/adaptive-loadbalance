a3e71b945e1f91b000f67734c8a09ee4a35527a8
liliang <liliang_68@126.com>
2019-06-19 20:40:12 +0800

    修改启动脚本

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+mvn clean install -DskipTests
 nohup java -Dquota=small -jar  ./service-provider/target/service-provider.jar >small.txt 2>&1 &
 nohup java -Dquota=medium -jar  ./service-provider/target/service-provider.jar >medium.txt 2>&1 &
 nohup java -Dquota=large -jar  ./service-provider/target/service-provider.jar >large.txt 2>&1 &
