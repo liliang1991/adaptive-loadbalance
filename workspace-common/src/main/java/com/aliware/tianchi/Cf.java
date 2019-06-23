db090c830b04ffb7fcddd32839eba71638ca96b7
liliang1991 <liliang_68@126.com>
2019-06-23 23:55:28 +0800

    代码优化

---
+++ workspace-common/src/main/java/com/aliware/tianchi/Cf.java
@@ -0,0 +1,10 @@
+package com.aliware.tianchi;
+
+import org.apache.dubbo.rpc.Invoker;
+
+import java.util.concurrent.CompletableFuture;
+
+public class Cf {
+    static CompletableFuture<Invoker> completableFuture;
+
+}
