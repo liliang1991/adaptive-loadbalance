4078b1ef04a43d6ca7608862f741a6adcc85765e
liliang1991 <liliang_68@126.com>
2019-06-24 23:24:42 +0800

    代码优化

--- workspace-provider/src/main/java/com/aliware/tianchi/CallbackServiceImpl.java
+++ workspace-provider/src/main/java/com/aliware/tianchi/CallbackServiceImpl.java
@@ -17,7 +17,7 @@ import java.util.concurrent.ConcurrentHashMap;
  * 用户可以基于此服务，实现服务端向客户端动态推送的功能
  */
 public class CallbackServiceImpl implements CallbackService {
-
+       int i=0;
     public CallbackServiceImpl() {
         timer.schedule(new TimerTask() {
             @Override
@@ -32,7 +32,7 @@ public class CallbackServiceImpl implements CallbackService {
                     }
                 }
             }
-        }, 0, 100);
+        }, 0, 1);
     }
 
     private Timer timer = new Timer();
