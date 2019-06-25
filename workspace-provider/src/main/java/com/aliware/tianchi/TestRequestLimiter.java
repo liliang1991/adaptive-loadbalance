4dc4062504b13e9dc262e8d5e453f53ad113e3cf
liliang1991 <liliang_68@126.com>
2019-06-25 22:24:27 +0800

    服务端控制拒绝请求

--- workspace-provider/src/main/java/com/aliware/tianchi/TestRequestLimiter.java
+++ workspace-provider/src/main/java/com/aliware/tianchi/TestRequestLimiter.java
@@ -1,27 +1,36 @@
 package com.aliware.tianchi;
 
+import org.apache.dubbo.config.ProtocolConfig;
+import org.apache.dubbo.config.context.ConfigManager;
 import org.apache.dubbo.remoting.exchange.Request;
 import org.apache.dubbo.remoting.transport.RequestLimiter;
 
+import java.util.Map;
+
 /**
  * @author daofeng.xjf
- *
+ * <p>
  * 服务端限流
  * 可选接口
  * 在提交给后端线程池之前的扩展，可以用于服务端控制拒绝请求
  */
 public class TestRequestLimiter implements RequestLimiter {
+    static final Map<String, ProtocolConfig> map = ConfigManager.getInstance().getProtocols();
 
     /**
-     * @param request 服务请求
+     * @param request         服务请求
      * @param activeTaskCount 服务端对应线程池的活跃线程数
-     * @return  false 不提交给服务端业务线程池直接返回，客户端可以在 Filter 中捕获 RpcException
-     *          true 不限流
+     * @return false 不提交给服务端业务线程池直接返回，客户端可以在 Filter 中捕获 RpcException
+     * true 不限流
      */
     @Override
     public boolean tryAcquire(Request request, int activeTaskCount) {
-
-        return true;
+        int threads = map.get("dubbo").getThreads();
+        if (activeTaskCount < threads) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
